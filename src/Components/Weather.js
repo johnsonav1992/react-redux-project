845dd7d530ae80cd8c97b0a769b4e9244f857544
Alex Johnson <johnsonav1992@gmail.com>
2022-10-02 17:45:32 -0500

    finish project. Clean-up details in a few days

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,34 +1,72 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { selectDisplay } from '../redux/slices/displayCountrySlice'
+import { setLoading } from '../redux/slices/loadingSlice'
 
 const Weather = () => {
-    const [weather, setWeather] = useState();
+	const [weather, setWeather] = useState()
+	const display = useSelector(selectDisplay)
+    const isLoading = useSelector(state => state.loading.isLoading)
+    const dispatch = useDispatch()
+	let latitude = display.capitalInfo.latlng[0]
+	let longitude = display.capitalInfo.latlng[1]
 
-    // ------------------------------------
-    // PASTE RAPIDAPI CODE SNIPPET IN A USEEFFECT HERE
-    // ------------------------------------
+	useEffect(() => {
+        dispatch(setLoading(true))
+		const options = {
+			method: 'GET',
+			url: 'https://weatherapi-com.p.rapidapi.com/current.json',
+			params: { q: `${latitude}, ${longitude}` },
+			headers: {
+				'X-RapidAPI-Key':
+					'da34692678msh65bb81a5b33e893p11a4f0jsn830e53009611',
+				'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com',
+			},
+		}
 
-    return (
-        <div>
-            <table className="overview-table">
-                <tr>
-                    <td>Conditions: </td>
-                </tr>
-                <tr>
-                    <td>Temperature: </td>
-                </tr>
-                <tr>
-                    <td>Feels Like: </td>
-                </tr>
-                <tr>
-                    <td>Humidity: </td>
-                </tr>
-                <tr>
-                    <td>Wind Speed: </td>
-                </tr>
-            </table>
-        </div>
-    );
-};
+		axios
+			.request(options)
+			.then(function (response) {
+				console.log(response.data)
+				setWeather(response.data)
+                dispatch(setLoading(false))
+			})
+			.catch(function (error) {
+				console.error(error)
+			})
+	}, [dispatch, latitude, longitude])
 
-export default Weather;
+	return (
+		<div>
+            {isLoading ? <p>Loading...</p> : <table className="overview-table">
+				<tr>
+					<td>Conditions: </td>
+					<td>{weather?.current?.condition?.text}</td>
+				</tr>
+				<tr>
+					<td>Temperature: </td>
+					<td>{weather?.current?.temp_f} degrees Fahrenheit</td>
+				</tr>
+				<tr>
+					<td>Feels Like: </td>
+					<td>{weather?.current?.feelslike_f} degrees Fahrenheit</td>
+				</tr>
+				<tr>
+					<td>Humidity: </td>
+					<td>{weather?.current?.humidity}%</td>
+				</tr>
+				<tr>
+					<td>Wind Speed: </td>
+					<td>
+						{weather?.current?.wind_mph} mph{' '}
+						{weather?.current?.wind_dir}
+					</td>
+				</tr>
+			</table>}
+			
+		</div>
+	)
+}
+
+export default Weather
